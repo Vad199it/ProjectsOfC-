f10b311f68850c3305c2affd8ce3a702654e2ad9
Минин А.В <MA@dc.transoil.gomel.by>
2020-07-20 13:18:08 +0300

    feat: add recurs and lambda

---
+++ LambdaExpression/LambdaExpression/LambdaExpression.cpp
@@ -0,0 +1,33 @@
+﻿#include <string_view>
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+struct Season
+{
+    std::string_view name{};
+    double averageTemperature{};
+};
+
+
+int main()
+{
+    std::array<Season, 4> seasons{
+      { { "Spring", 285.0 },
+        { "Summer", 296.0 },
+        { "Fall", 288.0 },
+        { "Winter", 263.0 } }
+    };
+
+    std::sort(seasons.begin(), seasons.end(),
+        [](const auto& a, const auto& b) {
+            return (a.averageTemperature < b.averageTemperature);
+        });
+
+    for (const auto& season : seasons)
+    {
+        std::cout << season.name << '\n';
+    }
+
+    return 0;
+}
---
+++ Recursion/Recursion/Recursion.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int factorial(int number){
+	if (number == 0) return 1;
+	return factorial(number - 1) * number;
+}
+
+int summValueOfNumber(int number) {
+	if (number < 10) return number;
+	return summValueOfNumber(number / 10) + number % 10;
+}
+
+void getBinary(unsigned int number) {
+	if (number == 0) return;
+
+	getBinary(number / 2);
+
+	std::cout << number % 2;
+}
+
+void printData(int number) {
+	if (number == 0)  std::cout << 0;
+	else
+	getBinary(static_cast<unsigned int>(number));
+}
+
+int main()
+{
+	printData(-20);
+}
+
