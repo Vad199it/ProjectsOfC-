f749dd69aefb12f1da83490749925f56672cccc2
Минин А.В <MA@dc.transoil.gomel.by>
2020-07-21 13:26:17 +0300

    feat: add class Stack

---
+++ ClassStack/ClassStack/ClassStack.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <array>
+#include <cassert>
+
+class Stack {
+public:
+    std::array <int, 10> arr;
+    int length;
+
+    void reset() {
+        arr = { 0 };
+        length = 0;
+    }
+
+    bool push(int value) {
+        if (length < 11) {
+            arr[length++] = value;
+            return true;
+        }
+        else return false;
+            
+    }
+
+    int pop() {
+        assert(length > 0 && "Not value");
+        return arr[--length];
+    }
+
+    void print() {
+        std::cout << "(";
+        for (int count(0); count < length; ++count)
+            std::cout << " " << arr[count] << " ";
+            std::cout << ")";
+
+    }
+};
+
+int main()
+{
+    Stack stack;
+    stack.reset();
+
+    stack.print();
+
+    stack.push(3);
+    stack.push(7);
+    stack.push(5);
+    stack.print();
+
+    stack.pop();
+    stack.print();
+
+    stack.pop();
+    stack.pop();
+
+    stack.print();
+
+    return 0;
+  
+}
+
---
+++ FirstClass/FirstClass/FirstClass.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+class Numbers {
+public:
+    int m_firstNumber;
+    int m_secondNumber;
+
+    void set(int firstNumber, int secondNumber) {
+        m_firstNumber = firstNumber;
+        m_secondNumber = secondNumber;
+    }
+
+    void print() {
+        std::cout << m_firstNumber << " " << m_secondNumber;
+    }
+};
+
+int main()
+{
+    Numbers n1;
+    n1.set(3, 3);
+
+    Numbers n2{ 4, 4 };
+
+    n1.print();
+    n2.print();
+
+    return 0;
+}
+
